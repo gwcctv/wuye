e80688ca1c472832c0cc54e9707a6562f5eb3c55
xiaowenpeng <793627230@qq.com>
2022-10-19 18:45:18 +0800

    xxxx

---
+++ batchservice/src/main/java/com/woniuxy/batchservice/provider/TbcycleProvider.java
@@ -0,0 +1,50 @@
+package com.woniuxy.batchservice.provider;
+
+import com.woniuxy.wuye.common.entity.TbCycle;
+import lombok.extern.slf4j.Slf4j;
+import org.apache.ibatis.jdbc.SQL;
+
+@Slf4j
+public class TbcycleProvider {
+    public String selectByCondition(TbCycle tbCycle){
+        String sql=new SQL(){
+            {
+                SELECT("*");
+                FROM("(SELECT e.*,f.number building_number FROM (SELECT c.*,d.project_name FROM (SELECT a.*,b.project_id,b.building_id,b.unit,b.layer,b.house_number FROM   (SELECT c.*,i.item_name FROM tb_cycle c LEFT JOIN tb_chargeable_items i ON c.feetype_id=i.id) a LEFT JOIN tb_house b ON a.house_id=b.house_id) c LEFT JOIN tb_project d ON c.project_id=d.project_id) e LEFT JOIN tb_building f ON e.building_id=f.building_id) g");
+                if(tbCycle.getProjectName()!=null && !tbCycle.getProjectName().equals("")){
+                    WHERE("project_name=#{projectName}");
+                }
+                if(tbCycle.getItemName()!=null && !tbCycle.getItemName().equals("")){
+                    WHERE("item_name=#{itemName}");
+                }
+                if(tbCycle.getProduceStatus()!=null && !tbCycle.getProduceStatus().equals("")){
+                    WHERE("produce_status=#{produceStatus}");
+                }
+                if(tbCycle.getStartYear()!=null  && !tbCycle.getStartYear().equals("")){
+                    WHERE("start_year=#{startYear}");
+                }   if(tbCycle.getStartMonth()!=null  && !tbCycle.getStartMonth().equals("")){
+                WHERE("start_month=#{startMonth}");
+            }   if(tbCycle.getStartDay()!=null  && !tbCycle.getStartDay().equals("")){
+                WHERE("start_day=#{startDay}");
+            }
+                if(tbCycle.getEndYear()!=null && !tbCycle.getEndYear().equals("")){
+                    WHERE("end_year=#{endYear}");
+                }
+                if(tbCycle.getEndMonth()!=null && !tbCycle.getEndMonth().equals("")){
+                    WHERE("end_month=#{endMonth}");
+                }
+                if(tbCycle.getEndDay()!=null && !tbCycle.getEndDay().equals("")){
+                    WHERE("end_day=#{endDay}");
+                }
+                if(tbCycle.getProduceName()!=null && !tbCycle.getProduceName().equals("")){
+                    WHERE("produce_name=#{produceName}");
+                }
+                if(tbCycle.getProduceTime()!=null  && !tbCycle.getProduceTime().equals("")){
+                    WHERE("produce_time=#{produceTime}");
+                }
+            }
+
+        }.toString();
+        return  sql;
+    }
+}
--- common/src/main/java/com/woniuxy/wuye/common/entity/TbCycle.java
+++ common/src/main/java/com/woniuxy/wuye/common/entity/TbCycle.java
@@ -1,5 +1,6 @@
 package com.woniuxy.wuye.common.entity;
 
+import com.baomidou.mybatisplus.annotation.*;
 import lombok.Data;
 
 import java.io.Serializable;
@@ -9,76 +10,138 @@ import java.util.Date;
  * 循环周期业务实体类
  */
 @Data
+@TableName("tb_cycle")
 public class TbCycle implements Serializable {
 
     /**
      * 主键
      */
+    @TableId(value = "id",type = IdType.AUTO )
     private Integer id;
 
-    /**
-     * 项目ID
-     */
-    private Integer projectId;
+
+
 
     /**
      * 收费项目ID
      */
+   @TableField(value = "feetype_id",condition = SqlCondition.EQUAL)
     private Integer feetypeId;
 
     /**
      * 房产ID
      */
+    @TableField(value = "house_id",condition = SqlCondition.EQUAL)
     private Integer houseId;
 
     /**
      * 收费周期
      */
+@TableField(condition = SqlCondition.EQUAL)
     private Integer feecycle;
 
     /**
      * 周期收费开始时间
      */
-    private Date start;
-
+@TableField(condition = SqlCondition.EQUAL)
+    private Date startYear;
+    /**
+     * 周期收费开始时间
+     */
+    @TableField(condition = SqlCondition.EQUAL)
+    private Date startMonth;
+    /**
+     * 周期收费开始时间
+     */
+    @TableField(condition = SqlCondition.EQUAL)
+    private Date startDay;
     /**
      * 周期收费停止时间
      */
-    private Date end;
-
+    @TableField(condition = SqlCondition.EQUAL)
+    private Date endYear;
+    /**
+     * 周期收费停止时间
+     */
+    @TableField(condition = SqlCondition.EQUAL)
+    private Date endMonth;
+    /**
+     * 周期收费停止时间
+     */
+    @TableField(condition = SqlCondition.EQUAL)
+    private Date endDay;
     /**
      * 创建状态
      */
+    @TableField(value = "produce_status",condition = SqlCondition.EQUAL)
     private String produceStatus;
 
     /**
      * 创建人
      */
+    @TableField(value = "produce_name",condition = SqlCondition.EQUAL)
     private String produceName;
 
     /**
      * 创建时间
      */
+    @TableField(value = "produce_time",condition = SqlCondition.EQUAL)
     private Date produceTime;
 
     /**
      * 业务批次号
      */
+    @TableField(condition = SqlCondition.EQUAL)
     private String number;
 
     /**
      * 单价
      */
+    @TableField(condition = SqlCondition.EQUAL)
     private String price;
 
     /**
      * 应收    单价*周期
      */
+
+    @TableField(value = "total_money",condition = SqlCondition.EQUAL)
     private String totalMoney;
 
     /**
      * 生成失败的原因 比如逻辑问题 截止时间早于起始时间
      */
+    @TableField(value = "failure_produce_reason",condition = SqlCondition.LIKE)
     private String failureProduceReason;
 
+
+    /**
+     * 收费项目名称
+     * */
+    private String itemName;
+
+    /**
+     * 单元号
+     * */
+    private String unit;
+
+/**
+ * 楼层
+ * */
+    private String layer;
+
+/**
+ * 房号
+ * */
+    private String houseNumber;
+
+/**
+ * 项目名
+ * */
+    private String projectName;
+
+
+    /**
+     * 楼栋号
+     * */
+    private String  buildingNumber;
 }
