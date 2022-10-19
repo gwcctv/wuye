e509fb8ae441474580d9c6d2cf928cf3926b61c3
sssspike <962126029@qq.com>
2022-10-19 09:53:55 +0800

    实体类

---
+++ common/src/main/java/com/woniuxy/wuye/common/entity/TbBuilding.java
@@ -0,0 +1,45 @@
+package com.woniuxy.wuye.common.entity;
+
+/**
+ * 物业楼栋实体类
+ */
+public class TbBuilding {
+    /**
+     * 楼栋主键id
+     */
+    private Integer buildingId;
+    /**
+     * 楼栋编号
+     */
+    private String number;
+
+    /**
+     *楼栋号
+     */
+    private Integer buildingNumber;
+
+    /**
+     * 楼栋类型
+     */
+    private String buildingType;
+
+    /**
+     * 层数
+     */
+    private String layer;
+
+    /**
+     * 单元数
+     */
+    private String unit;
+
+    /**
+     * 备注
+     */
+    private String description;
+
+    /**
+     * 所属项目id
+     */
+    private Integer projectId;
+}
