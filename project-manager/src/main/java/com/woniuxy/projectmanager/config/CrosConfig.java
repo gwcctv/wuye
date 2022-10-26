bd8f2326e3b138edea80d420a0902f3ce62faac5
sssspike <962126029@qq.com>
2022-10-26 09:35:37 +0800

    更新接口

---
+++ project-manager/src/main/java/com/woniuxy/projectmanager/config/CrosConfig.java
@@ -0,0 +1,18 @@
+package com.woniuxy.projectmanager.config;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+@Configuration
+class CorsConfig implements WebMvcConfigurer {
+    @Override
+    public void addCorsMappings(CorsRegistry registry) {
+        registry.addMapping("/**").
+                allowCredentials(true).
+                allowedHeaders("*").
+                allowedOrigins("*").
+
+                allowedMethods("*");
+    }
+}
