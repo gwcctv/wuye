aee023c995a5b09eec842ba9af7a98b88aa906da
魏锦鹏 <sdkjdxwjp@163.com>
2022-10-26 20:33:24 +0800

    10/26

--- finance-business-configuration/src/main/java/com/woniuxy/busconfig/web/TbChargeTypeController.java
+++ finance-business-configuration/src/main/java/com/woniuxy/busconfig/web/TbChargeTypeController.java
@@ -1,11 +1,24 @@
 package com.woniuxy.busconfig.web;
 
 
+import com.alibaba.fastjson.JSON;
 import com.alibaba.fastjson.JSONObject;
 import com.woniuxy.busconfig.service.TbChargeTypeService;
 import com.woniuxy.wuye.common.entity.TbChargeType;
 import com.woniuxy.wuye.common.utils.ResponseEntity;
 import lombok.extern.slf4j.Slf4j;
+import org.apache.http.HttpHost;
+import org.elasticsearch.action.search.SearchRequest;
+import org.elasticsearch.action.search.SearchResponse;
+import org.elasticsearch.client.RequestOptions;
+import org.elasticsearch.client.RestClient;
+import org.elasticsearch.client.RestHighLevelClient;
+import org.elasticsearch.common.text.Text;
+import org.elasticsearch.index.query.QueryBuilders;
+import org.elasticsearch.search.SearchHit;
+import org.elasticsearch.search.builder.SearchSourceBuilder;
+import org.elasticsearch.search.fetch.subphase.highlight.HighlightBuilder;
+import org.elasticsearch.search.fetch.subphase.highlight.HighlightField;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.redis.core.ListOperations;
 import org.springframework.data.redis.core.RedisTemplate;
@@ -14,9 +27,11 @@ import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.io.IOException;
 import java.util.ArrayList;
 import java.util.Iterator;
 import java.util.List;
+import java.util.Map;
 import java.util.stream.Collectors;
 
 /**
@@ -31,6 +46,8 @@ public class TbChargeTypeController {
     //引入redis
     @Autowired
     private RedisTemplate redisTemplate;
+    @Autowired
+   private RestHighLevelClient restHighLevelClient;
     @RequestMapping("/getAll")
     public ResponseEntity getTbChargeType(){
         List<TbChargeType> listAll =null;
@@ -117,4 +134,63 @@ public class TbChargeTypeController {
     public ResponseEntity addList(Integer id){
         return new ResponseEntity("200","ok",tbChargeTypeService.getById(id));
     }
+@RequestMapping("search")
+public ResponseEntity  search(String name) {
+
+    HttpHost httpHost = new HttpHost("localhost", 9200, "http");
+    restHighLevelClient = new RestHighLevelClient(RestClient.builder(httpHost));
+    //查询请求
+    SearchRequest searchRequest = new SearchRequest("tbchargetype");
+    //创建询条件
+    SearchSourceBuilder searchSourceBuilder = new SearchSourceBuilder();
+    //构造查询条件
+    searchSourceBuilder.query(QueryBuilders.multiMatchQuery(name, "name"));
+    //高亮显示
+    HighlightBuilder highlightBuilder=new HighlightBuilder().field("name")
+            .preTags("<span style='color:red'>").postTags("</span>");
+    searchSourceBuilder.highlighter(highlightBuilder);
+    //添加查询条件到查询请求
+    searchRequest.source(searchSourceBuilder);
+    //发送查询请求
+    SearchResponse response =
+            null;
+    try {
+        response = restHighLevelClient.search(searchRequest, RequestOptions.DEFAULT);
+    } catch (IOException e) {
+        throw new RuntimeException(e);
+    }
+    //解析JSON，将JSON转成对象
+    List<TbChargeType> tbChargeTypes =new ArrayList<>();
+    long value = response.getHits().getTotalHits().value;
+    if (value > 0) {
+        SearchHit[] hits = response.getHits().getHits();
+        for (SearchHit hit : hits) {
+            //获取到高亮字段
+            Map<String, HighlightField> highlightFields = hit.getHighlightFields();
+            //将高亮字段的具体内容取出来
+            HighlightField title = highlightFields.get("name");
+            Map<String, Object> map = hit.getSourceAsMap();
+            if (title != null) {
+                //得到高亮的字符串内容
+                Text[] fragments = title.fragments();
+                String n_title = "";
+                for (Text fragment : fragments) {
+                    n_title+=fragment;
+                }
+                //将原本没有高亮的结果集合中的title换为由高亮的
+                map.put("name",n_title);
+            }
+            //利用阿里解析包里的json解析方法解析为对象
+            TbChargeType obj = JSON.parseObject(JSON.toJSONString(map), TbChargeType.class);
+            tbChargeTypes.add(obj);
+        }}
+        try {
+            restHighLevelClient.close();
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
+    return new ResponseEntity("200", "ok", tbChargeTypes);
+    }
+
 }
+
